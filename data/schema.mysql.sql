d3bc1a00163c1570a5e2d1b8eb8e46d2b430f9e0
amaurylavieille <amaury.lavieille@gmail.com>
2014-03-01 15:41:46 +0100

    Ajout dossier data et schema base de données

---
+++ data/schema.mysql.sql
@@ -0,0 +1,7 @@
+/** Table exemple **/
+CREATE TABLE IF NOT EXISTS `Exemple` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `content` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB 
